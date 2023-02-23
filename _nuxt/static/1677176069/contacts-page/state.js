5e3694371696f6bf48a095de27544aa5de4d84ef
Klimova Natalya <natalykliini@gmail.com>
2023-02-23 23:19:51 +0500

    Updates

--- _nuxt/static/1677176069/contacts-page/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fbusiness-card-site\u002F_nuxt\u002Fstatic\u002F1677176069",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcontacts-page",config:{_app:{basePath:"\u002Fbusiness-card-site\u002F",assetsPath:"\u002Fbusiness-card-site\u002F_nuxt\u002F",cdnURL:a}}}}(null));
